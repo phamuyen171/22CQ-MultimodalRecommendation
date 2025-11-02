8a2b92797d397e4603aa068226b6cff3167dccbb
22120424 <22120424@student.hcmus.edu.vn>
2025-11-02 03:27:53 +0000

    running

--- train_clothing.sh
+++ train_clothing.sh
@@ -2,6 +2,8 @@
 #SBATCH --job-name=lgmrec_job
 #SBATCH --nodes=1
 #SBATCH --ntasks=4
+#SBATCH --cpus-per-task=4    
+#SBATCH --gres=gpu:4
 #SBATCH --time=02:00:00
 #SBATCH --output=slurm-%j.out  # Nên dùng %j để tên file output là JobID
 
@@ -12,5 +14,5 @@
 # python main.py -m LGMRec -d sports -g 0
 
 
-python preprocessing/dualgnn-gen-u-u-matrix.py -d clothing
+# python preprocessing/dualgnn-gen-u-u-matrix.py -d clothing
 python main.py -m LGMRec -d clothing -g 0
